884806d813b9f2c16985aeabe08de6f3e5b2b17e
bloodborne <vijayrmourya@gmail.com>
2021-10-15 11:34:07 +0530

    git

--- GitClone.groovy
+++ GitClone.groovy
@@ -5,7 +5,7 @@ pipeline {
         stage('Git-Clone') {
             steps {
                 echo 'Hello Cloning your git'
-				git config --global --unset credential.helper
+				sh "git config --global --unset credential.helper"
                 git credentialsId: 'GitToken', url: 'https://github.com/vijayrmourya/_Projects'
                 echo 'clone successful'
             }
